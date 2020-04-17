702a149e718cdb1f7f6ccf3e21a84f79f55a2df2
17130120142 <61042147+17130120142@users.noreply.github.com>
2020-04-17 13:57:08 +0800

    加入项目之菜单框架

---
+++ src/components/Join.vue
@@ -0,0 +1,107 @@
+<template>
+<div>
+ <el-container>
+
+   <el-header height="100px">
+     <img src="@/assets/logo.png":width='144' height="100">
+     <el-button type="info" @click="logout">张三  注销</el-button>
+   </el-header>
+
+   <el-container >
+     <el-aside width="300px">
+         <el-button class="left_menu" type="text" icon="el-icon-back"></el-button>
+         <br>
+         <el-button class="left_menu" type="text" icon="el-icon-loading">进行中项目</el-button>
+         <br>
+         <el-button class="left_menu" type="text" icon="el-icon-finished">已完成项目</el-button>
+         <br>
+         <el-button class="left_menu" type="text" icon="el-icon-user-solid">个人中心</el-button>
+         <br>
+     </el-aside>
+     <el-main>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">全部项目</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '/' }">我加入的项目</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '/' }">加入项目</el-breadcrumb-item>
+        </el-breadcrumb>
+     </el-main>
+   </el-container>
+ </el-container>
+</div>
+
+</template>
+
+<script>
+  var log = 1;
+  export default
+  {
+      username: "李四",
+      data()
+      {
+        
+      },
+        methods: 
+        {
+            logout() 
+            {
+                log = 0;
+                if(log==0)
+                this.$alert('您已注销登录！', 'Logout', {
+                    confirmButtonText: '确定',
+                    callback: action => {
+                    this.$message({
+                    type: 'info',
+                    message: `action: ${ action }`
+                    });
+                    }
+                    });
+            },
+        }
+  }
+</script>
+
+<style>
+  .el-header, .el-footer {
+    background-color: rgb(255, 255, 255);
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #5061CB;
+    color: rgb(255, 255, 255);
+    text-align: center;
+    line-height: 70px;
+    height: 800px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+
+</style>
+
+<style>
+.left_menu
+{
+  float:left;
+  background-color:#5061CB;
+  color:#FFFFFF;
+  border-style:none;
+  font-size:20px;
+}
+
+.el-breadcrumb
+{
+  font-size:20px;
+}
+</style>
